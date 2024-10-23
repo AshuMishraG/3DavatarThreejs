d1cdba471b9b08eaf4cde704ce807fe95d8f5bdf
AshuMishraG <aashutoshm77@gmail.com>
2024-10-23 18:00:04 +0530

    Camera Position Fixed

--- src/components/Experiance.jsx
+++ src/components/Experiance.jsx
@@ -1,17 +1,40 @@
 "use client";
-import { Box, Environment, Gltf, OrbitControls } from "@react-three/drei";
+import { Box, CameraControls, Environment, Gltf, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 export const Experiance = () => {
    return (
       <>
-         <Canvas>
-            <OrbitControls />
+         <Canvas
+            camera={{ 
+               position: [0, 0, 0.0001],
+            }}
+         >
+            <CameraManager />
             <Environment preset="sunset" />
             <ambientLight intensity={0.8} color="pink" />
-            <pointLight position={[10, 10, 10]} />
-            <Gltf src="/models/classroom_default.glb" />
+            <Gltf src="/models/classroom_default.glb" position={[0.2, -1.7, -2]} />
          </Canvas>
       </>
+   );
+};
+
+const CameraManager = () => {
+   return (
+      <CameraControls
+      //   ref={controls}
+        minZoom={1}
+        maxZoom={3}
+        polarRotateSpeed={-0.3} // REVERSE FOR NATURAL EFFECT
+        azimuthRotateSpeed={-0.3} // REVERSE FOR NATURAL EFFECT
+        mouseButtons={{
+          left: 1, //ACTION.ROTATE
+          wheel: 16, //ACTION.ZOOM
+        }}
+        touches={{
+          one: 32, //ACTION.TOUCH_ROTATE
+          two: 512, //ACTION.TOUCH_ZOOM
+        }}
+      />
    );
 };
