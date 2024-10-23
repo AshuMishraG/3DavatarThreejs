7b7b5856a93de7747962df2a5b08189a6990b9cf
AshuMishraG <aashutoshm77@gmail.com>
2024-10-23 18:36:10 +0530

    Teacher model added

--- src/components/Experiance.jsx
+++ src/components/Experiance.jsx
@@ -1,19 +1,36 @@
 "use client";
-import { Box, CameraControls, Environment, Gltf, OrbitControls } from "@react-three/drei";
+import {
+   Box,
+   CameraControls,
+   Environment,
+   Gltf,
+   OrbitControls,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Teacher } from "./teacher";
+import { degToRad } from "three/src/math/MathUtils";
 
 export const Experiance = () => {
    return (
       <>
          <Canvas
-            camera={{ 
+            camera={{
                position: [0, 0, 0.0001],
             }}
          >
             <CameraManager />
             <Environment preset="sunset" />
             <ambientLight intensity={0.8} color="pink" />
-            <Gltf src="/models/classroom_default.glb" position={[0.2, -1.7, -2]} />
+            <Teacher
+               teacher={"Nanami"}
+               position={[-1, -1.7, -3]}
+               scale={1.5}
+               rotation-y={degToRad(20)}
+            />
+            <Gltf
+               src="/models/classroom_default.glb"
+               position={[0.2, -1.7, -2]}
+            />
          </Canvas>
       </>
    );
@@ -22,19 +39,19 @@ export const Experiance = () => {
 const CameraManager = () => {
    return (
       <CameraControls
-      //   ref={controls}
-        minZoom={1}
-        maxZoom={3}
-        polarRotateSpeed={-0.3} // REVERSE FOR NATURAL EFFECT
-        azimuthRotateSpeed={-0.3} // REVERSE FOR NATURAL EFFECT
-        mouseButtons={{
-          left: 1, //ACTION.ROTATE
-          wheel: 16, //ACTION.ZOOM
-        }}
-        touches={{
-          one: 32, //ACTION.TOUCH_ROTATE
-          two: 512, //ACTION.TOUCH_ZOOM
-        }}
+         //   ref={controls}
+         minZoom={1}
+         maxZoom={3}
+         polarRotateSpeed={-0.3} // REVERSE FOR NATURAL EFFECT
+         azimuthRotateSpeed={-0.3} // REVERSE FOR NATURAL EFFECT
+         mouseButtons={{
+            left: 1, //ACTION.ROTATE
+            wheel: 16, //ACTION.ZOOM
+         }}
+         touches={{
+            one: 32, //ACTION.TOUCH_ROTATE
+            two: 512, //ACTION.TOUCH_ZOOM
+         }}
       />
    );
 };
